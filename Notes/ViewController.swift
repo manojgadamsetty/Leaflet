c714669926ef08b27984da34b2fde3ef3cd18556
Manoj Gadamsetty <manojgadamsetty@gmail.com>
2025-07-18 21:52:47 +0530

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Manoj Gadamsetty on 18/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
